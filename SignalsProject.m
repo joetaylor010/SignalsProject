ae17e6f9b342e3a5a25a26f64943199e76ca6345
Joe Taylor <44660221+joetaylor010@users.noreply.github.com>
2018-12-05 13:04:39 -0500

    Update SignalsProject.m
    Added FFT calculation and graphing.
    Improved repeat efficiency of file reads.

--- SignalsProject.m
+++ SignalsProject.m
@@ -11,42 +11,59 @@ avgcurrent2 = zeros(14,1);
 voltage = [0 0.1 0.5 1 1.5 2 3 4 5 6 7 8 9 10];
 
 % Load device 1 data
-device1(1,:) = load('Device-1\CURRENT-0.0V.dat')';
-device1(2,:) = load('Device-1\CURRENT-0.1V.dat')';
-device1(3,:) = load('Device-1\CURRENT-0.5V.dat')';
-device1(4,:) = load('Device-1\CURRENT-1.0V.dat')';
-device1(5,:) = load('Device-1\CURRENT-1.5V.dat')';
-device1(6,:) = load('Device-1\CURRENT-2.0V.dat')';
-device1(7,:) = load('Device-1\CURRENT-3.0V.dat')';
-device1(8,:) = load('Device-1\CURRENT-4.0V.dat')';
-device1(9,:) = load('Device-1\CURRENT-5.0V.dat')';
-device1(10,:) = load('Device-1\CURRENT-6.0V.dat')';
-device1(11,:) = load('Device-1\CURRENT-7.0V.dat')';
-device1(12,:) = load('Device-1\CURRENT-8.0V.dat')';
-device1(13,:) = load('Device-1\CURRENT-9.0V.dat')';
-device1(14,:) = load('Device-1\CURRENT-10.0V.dat')';
+if exist('device1','var') == 0
+    device1(1,:) = load('Device-1\CURRENT-0.0V.dat')';
+    device1(2,:) = load('Device-1\CURRENT-0.1V.dat')';
+    device1(3,:) = load('Device-1\CURRENT-0.5V.dat')';
+    device1(4,:) = load('Device-1\CURRENT-1.0V.dat')';
+    device1(5,:) = load('Device-1\CURRENT-1.5V.dat')';
+    device1(6,:) = load('Device-1\CURRENT-2.0V.dat')';
+    device1(7,:) = load('Device-1\CURRENT-3.0V.dat')';
+    device1(8,:) = load('Device-1\CURRENT-4.0V.dat')';
+    device1(9,:) = load('Device-1\CURRENT-5.0V.dat')';
+    device1(10,:) = load('Device-1\CURRENT-6.0V.dat')';
+    device1(11,:) = load('Device-1\CURRENT-7.0V.dat')';
+    device1(12,:) = load('Device-1\CURRENT-8.0V.dat')';
+    device1(13,:) = load('Device-1\CURRENT-9.0V.dat')';
+    device1(14,:) = load('Device-1\CURRENT-10.0V.dat')';
+end
+
 
 % Load device 2 data
-device2(1,:) = load('Device-2\CURRENT-0.0V.dat')';
-device2(2,:) = load('Device-2\CURRENT-0.1V.dat')';
-device2(3,:) = load('Device-2\CURRENT-0.5V.dat')';
-device2(4,:) = load('Device-2\CURRENT-1.0V.dat')';
-device2(5,:) = load('Device-2\CURRENT-1.5V.dat')';
-device2(6,:) = load('Device-2\CURRENT-2.0V.dat')';
-device2(7,:) = load('Device-2\CURRENT-3.0V.dat')';
-device2(8,:) = load('Device-2\CURRENT-4.0V.dat')';
-device2(9,:) = load('Device-2\CURRENT-5.0V.dat')';
-device2(10,:) = load('Device-2\CURRENT-6.0V.dat')';
-device2(11,:) = load('Device-2\CURRENT-7.0V.dat')';
-device2(12,:) = load('Device-2\CURRENT-8.0V.dat')';
-device2(13,:) = load('Device-2\CURRENT-9.0V.dat')';
-device2(14,:) = load('Device-2\CURRENT-10.0V.dat')';
+if exist('device2','var') == 0
+    device2(1,:) = load('Device-2\CURRENT-0.0V.dat')';
+    device2(2,:) = load('Device-2\CURRENT-0.1V.dat')';
+    device2(3,:) = load('Device-2\CURRENT-0.5V.dat')';
+    device2(4,:) = load('Device-2\CURRENT-1.0V.dat')';
+    device2(5,:) = load('Device-2\CURRENT-1.5V.dat')';
+    device2(6,:) = load('Device-2\CURRENT-2.0V.dat')';
+    device2(7,:) = load('Device-2\CURRENT-3.0V.dat')';
+    device2(8,:) = load('Device-2\CURRENT-4.0V.dat')';
+    device2(9,:) = load('Device-2\CURRENT-5.0V.dat')';
+    device2(10,:) = load('Device-2\CURRENT-6.0V.dat')';
+    device2(11,:) = load('Device-2\CURRENT-7.0V.dat')';
+    device2(12,:) = load('Device-2\CURRENT-8.0V.dat')';
+    device2(13,:) = load('Device-2\CURRENT-9.0V.dat')';
+    device2(14,:) = load('Device-2\CURRENT-10.0V.dat')';
+end
+n = 130001;
+fshift = -65000:1:65000;
 
-% Calculate average current for both devices
+fourier1 = fft(device1,[],2);
+fourier2 = fft(device2,[],2);
+fouriershift1 = fftshift(fourier1);
+fouriershift2 = fftshift(fourier2);
+
+% Calculate average current and fft for both devices
 for i=1:1:14
     avgcurrent1(i) = mean(device1(i,:));
     avgcurrent2(i) = mean(device2(i,:));
 end
+
+fshifttranspose1 = fouriershift1.';
+fshifttranspose2 = fouriershift2.';
+[maxamp1,maxindex1] = max(fshifttranspose1);
+[maxamp2,maxindex2] = max(fshifttranspose2);
 
 % Graph average current versus voltage for both devices
 figure
@@ -55,3 +72,23 @@ title('Average Current vs. Voltage (I-V Curve) for Devices 1 and 2')
 xlabel('Voltage (V)')
 ylabel('Average Current (A)')
 legend('Device 1', 'Device 2','Location','north')
+
+%Graph FFT of Device 1
+figure
+for i=1:1:14
+    subplot(5,3,i)
+    plot(fshift(64000:10:66000),abs(fouriershift1(i,64000:10:66000)))
+    title(['Current vs. Frequency for Device 1 at ',num2str(voltage(i)),' Volts'])
+    xlabel('Frequency (Hz)')
+    ylabel('Current (A)')
+end
+
+%Graph FFT of Device 2
+figure
+for i=1:1:14
+    subplot(5,3,i)
+    plot(fshift(64000:10:66000),abs(fouriershift2(i,64000:10:66000)))
+    title(['Current vs. Frequency for Device 2 at ',num2str(voltage(i)),' Volts'])
+    xlabel('Frequency (Hz)')
+    ylabel('Current (A)')
+end
