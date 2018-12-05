5b004e39cdcdd6c4673dd54612795b4d1c73c095
Thomas Tawadros <thomastawadros321@gmail.com>
2018-12-05 15:18:49 -0500

    BASIC DECENCY

--- SpectrumAnalyzer.m
+++ SpectrumAnalyzer.m
@@ -1,18 +1,18 @@
 function maxfreq = SpectrumAnalyzer(InputArray,Fs)
-Yaxis=abs(fftshift(fft(InputArray)));
-Yaxis=Yaxis.*Yaxis;
+Yaxis = abs(fftshift(fft(InputArray)));
+Yaxis = Yaxis.*Yaxis;
 
-v=size(Yaxis);
-if v(1)==1 
- V=size(Yaxis,2);
- Xaxis=-Fs/2+Fs/V:Fs/V:(Fs/2);
+v = size(Yaxis);
+if v(1)== 1
+ V = size(Yaxis,2);
+ Xaxis = -Fs/2+Fs/V:Fs/V:(Fs/2);
 else
-  V=v(1); 
-  Xaxis=-Fs/2+Fs/V:Fs/V:(Fs/2);
-  Xaxis=Xaxis.';
+  V = v(1);
+  Xaxis = -Fs/2+Fs/V:Fs/V:(Fs/2);
+  Xaxis = Xaxis.';
 end
 Xaxis = Xaxis + Fs/2;
-Yaxis=10*log10(Yaxis*4/(V.*V));
+Yaxis = 10*log10(Yaxis*4/(V.*V));
 plot(Xaxis,Yaxis)
 xlabel('Frequency (Hz)')
 ylabel('Power (dB)')
